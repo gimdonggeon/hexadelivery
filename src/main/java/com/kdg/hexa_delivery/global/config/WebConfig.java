2ada1a595ddb2f32f5353e1e4f10de7f612d7c76
정가현 <kahyun0608@gmail.com>
2024-12-05 00:20:21 +0900

    Feat : 로그인 필터 구현

---
+++ src/main/java/com/kdg/hexa_delivery/global/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.kdg.hexa_delivery.global.config;
+
+import com.kdg.hexa_delivery.global.filter.LoginFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean loginFilter() {
+
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+}
