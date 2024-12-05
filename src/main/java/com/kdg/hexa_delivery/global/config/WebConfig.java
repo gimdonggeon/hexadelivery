6f6c240a2bb0fc449b2695da92ec5384b4437bec
정가현 <kahyun0608@gmail.com>
2024-12-05 20:10:38 +0900

    Fix : 에러발생시 handlerInterceptor로 돌아오는 현상 해결

--- src/main/java/com/kdg/hexa_delivery/global/config/WebConfig.java
+++ src/main/java/com/kdg/hexa_delivery/global/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.kdg.hexa_delivery.global.config;
 import com.kdg.hexa_delivery.global.interceptor.CustomerRoleInterceptor;
 import com.kdg.hexa_delivery.global.interceptor.LoginInterceptor;
 import com.kdg.hexa_delivery.global.interceptor.OwnerRoleInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -10,9 +11,35 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Bean
+    private static final String[] LOGIN_REQUIRED_PATH_PATTERNS = {"/api/**"};
+    private static final String[] LOGIN_EXCLUDE_PATH_PATTERNS = {"/api/users/signup", "/api/users/login"};
+    private static final String[] CUSTOMER_ROLE_REQUIRED_PATH_PATTERNS = {"/api/customers/**"};
+    private static final String[] OWNER_ROLE_REQUIRED_PATH_PATTERNS = {"/api/owners/**"};
+
+    private final LoginInterceptor loginInterceptor;
+    private final CustomerRoleInterceptor customerRoleInterceptor;
+    private final OwnerRoleInterceptor ownerRoleInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns(LOGIN_REQUIRED_PATH_PATTERNS)
+                .excludePathPatterns(LOGIN_EXCLUDE_PATH_PATTERNS)
+                .order(1);
+
+        registry.addInterceptor(customerRoleInterceptor)
+                .addPathPatterns(CUSTOMER_ROLE_REQUIRED_PATH_PATTERNS)
+                .order(2);
+
+        registry.addInterceptor(ownerRoleInterceptor)
+                .addPathPatterns(OWNER_ROLE_REQUIRED_PATH_PATTERNS)
+                .order(3);
+    }
+
+    //    @Bean
 //    public FilterRegistrationBean loginFilter() {
 //
 //        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
@@ -22,34 +49,5 @@ public class WebConfig implements WebMvcConfigurer {
 //
 //        return filterRegistrationBean;
 //    }
-    private static final String[] LOGIN_REQUIRED_PATH_PATTERNS = {"/**"};
-    private static final String[] LOGIN_EXCLUDE_PATH_PATTERNS = {"/api/users", "api/users/login"};
-    private static final String[] CUSTOMER_ROLE_REQUIRED_PATH_PATTERNS = {"/api/orders", "/api/orders/{orderId}/review"};
-    private static final String[] OWNER_ROLE_REQUIRED_PATH_PATTERNS = {"/api/stores/business", "/api/stores/{storeId}", "/api/stores", "/api/orders/{orderId}", "/api/stores/{storeId}/menus/*"};
 
-    private final LoginInterceptor loginInterceptor;
-    private final CustomerRoleInterceptor customerRoleInterceptor;
-    private final OwnerRoleInterceptor ownerRoleInterceptor;
-
-    @Autowired
-    public WebConfig(LoginInterceptor loginInterceptor, CustomerRoleInterceptor customerRoleInterceptor, OwnerRoleInterceptor ownerRoleInterceptor) {
-        this.loginInterceptor = loginInterceptor;
-        this.customerRoleInterceptor = customerRoleInterceptor;
-        this.ownerRoleInterceptor = ownerRoleInterceptor;
-    }
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns(LOGIN_REQUIRED_PATH_PATTERNS)
-                .excludePathPatterns(LOGIN_EXCLUDE_PATH_PATTERNS)
-                .order(Ordered.HIGHEST_PRECEDENCE);
-
-        registry.addInterceptor(customerRoleInterceptor)
-                .addPathPatterns(CUSTOMER_ROLE_REQUIRED_PATH_PATTERNS)
-                .order(Ordered.HIGHEST_PRECEDENCE+1);
-
-        registry.addInterceptor(ownerRoleInterceptor)
-                .addPathPatterns(OWNER_ROLE_REQUIRED_PATH_PATTERNS)
-                .order(Ordered.HIGHEST_PRECEDENCE+2);
-    }
 }
