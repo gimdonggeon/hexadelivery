7b38daebd8187742018c25654c180b44e95d1aa6
popcon1234 <popcon1234@naver.com>
2024-12-05 17:51:28 +0900

    Fix : 소비자와 가게 주인 주문 컨트롤러 분리

--- src/main/java/com/kdg/hexa_delivery/domain/order/service/OrderService.java
+++ src/main/java/com/kdg/hexa_delivery/domain/order/service/OrderService.java
@@ -15,9 +15,6 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-import java.util.List;
-
 @Service
 public class OrderService {
 
