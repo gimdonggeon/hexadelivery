b54f4fcda270d3d2f2d6a58d2c26ed1b59a251e7
taketheking <so4524@gmail.com>
2024-12-05 11:13:33 +0900

    Refactor : 가게 접근 권한 확인 메서드 static 설정

---
+++ src/main/java/com/kdg/hexa_delivery/domain/base/validation/Validation.java
@@ -0,0 +1,42 @@
+package com.kdg.hexa_delivery.domain.base.validation;
+
+import com.kdg.hexa_delivery.domain.base.enums.Role;
+import com.kdg.hexa_delivery.domain.user.entity.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+public class Validation {
+
+    /**
+     *  가게 접근권한 확인 메서드
+     *
+     * @param httpServletRequest  request 객체
+     *
+     * @return loginUser 로그인된 유저 정보
+     *
+     */
+    public static User validStoreAccess(HttpServletRequest httpServletRequest) {
+        HttpSession session = httpServletRequest.getSession(false);
+        User loginUser = (User) session.getAttribute("loginUser");
+
+        // 사장님이 아니면 에러 발생
+        if(loginUser.getRole() != Role.MERCHANT){
+            throw new RuntimeException("사장님이 아닙니다.");
+        }
+        return loginUser;
+    }
+
+    /**
+     *  본인가게 접근권한 확인 메서드
+     *
+     * @param storeId 가게 id
+     * @param loginUser 로그인 된 유저 정보
+     *
+     */
+    public static void validMyStoreAccess(Long storeId,User loginUser){
+        // 사장님의 본인 가게가 아니면 예외 발생
+        if(loginUser.getStoreList().stream().filter(store -> store.getStoreId().equals(storeId)).findAny().isEmpty()){
+            throw new RuntimeException("본인의 가게가 아닙니다.");
+        }
+    }
+}
