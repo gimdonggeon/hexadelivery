6c1277224e03185fcb79a0b92957ccd32fbeeb07
taketheking <so4524@gmail.com>
2024-12-06 18:42:03 +0900

    Feat : 메뉴에 이미지 기능 추가

--- src/main/java/com/kdg/hexa_delivery/domain/menu/dto/UpdateMenuRequestDto.java
+++ src/main/java/com/kdg/hexa_delivery/domain/menu/dto/UpdateMenuRequestDto.java
@@ -3,14 +3,17 @@ package com.kdg.hexa_delivery.domain.menu.dto;
 import lombok.Getter;
 
 @Getter
-public class updateMenuRequestDto {
+public class UpdateMenuRequestDto {
 
     private final String menuName;
 
     private final Integer price;
 
-    public updateMenuRequestDto(String menuName, Integer price) {
+    private final String description;
+
+    public UpdateMenuRequestDto(String menuName, Integer price, String description) {
         this.menuName = menuName;
         this.price = price;
+        this.description = description;
     }
 }
--- src/main/java/com/kdg/hexa_delivery/domain/menu/entity/Menu.java
+++ src/main/java/com/kdg/hexa_delivery/domain/menu/entity/Menu.java
@@ -2,9 +2,13 @@ package com.kdg.hexa_delivery.domain.menu.entity;
 
 import com.kdg.hexa_delivery.domain.base.entity.BaseEntity;
 import com.kdg.hexa_delivery.domain.base.enums.Status;
+import com.kdg.hexa_delivery.domain.image.entity.Image;
 import com.kdg.hexa_delivery.domain.store.entity.Store;
 import jakarta.persistence.*;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
@@ -22,6 +26,12 @@ public class Menu extends BaseEntity {
     private Integer price;
 
     @Column(nullable = false)
+    private String description;
+
+    @OneToMany
+    private List<Image> imageList = new ArrayList<>();
+
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Status status;
 
@@ -29,24 +39,42 @@ public class Menu extends BaseEntity {
     @JoinColumn(name = "store_id", nullable = false)
     private Store store;
 
-    public Menu(String name, Integer price, Status status, Store store) {
+    public Menu(String name, Integer price, String description, Status status, Store store) {
         this.name = name;
         this.price = price;
+        this.description = description;
         this.status = status;
-        this.store = store;
+        updateStore(store);
     }
 
     public Menu() {
 
     }
 
-    public void updateMenu(String name, Integer price){
+
+    public void updateStore(Store store) {
+        this.store = store;
+        store.addMenuList(this);
+    }
+
+    public void updateImageUrls(List<Image> imageUrls) {
+        this.imageList = imageUrls;
+    }
+
+    public void updateMenu(String name, Integer price, String description, List<Image> imageUrls) {
         if(name != null && !name.isEmpty()){
             this.name = name;
         }
         if (price != null && price > 0){
             this.price = price;
         }
+        if(description != null && !description.isEmpty()){
+            this.description = description;
+        }
+        if(imageUrls != null && !imageUrls.isEmpty()){
+            this.imageList = imageUrls;
+        }
+
     }
 
     public void updateStatus2Delete() {
