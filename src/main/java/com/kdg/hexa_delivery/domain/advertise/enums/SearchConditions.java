76afb87139e0e226b3195c01d56a68baf1b59b21
dirtbagss <dirtbagag4@gmail.com>
2024-12-08 02:47:53 +0900

    fix : 주석추가

--- src/main/java/com/kdg/hexa_delivery/domain/advertise/enums/SearchConditions.java
+++ src/main/java/com/kdg/hexa_delivery/domain/advertise/enums/SearchConditions.java
@@ -1,7 +1,9 @@
 package com.kdg.hexa_delivery.domain.advertise.enums;
 
 // 사업자 검색 기준
-
+// REVIEW :: 리뷰슨
+// SCOPE :: 별점순
+// RECENTLY :: 최근개업한 가게
 public enum SearchConditions {
     REVIEW, SCOPE, RECENTLY
 }
