1da27efcc89b36c980780977575b2bf1e48f978c
dirtbagss <dirtbagag4@gmail.com>
2024-12-08 23:06:18 +0900

    fix : jpa 문법에 맞지않는 메서드 수정

--- src/main/java/com/kdg/hexa_delivery/domain/store/repository/StoreRepository.java
+++ src/main/java/com/kdg/hexa_delivery/domain/store/repository/StoreRepository.java
@@ -16,9 +16,6 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
     // 사업자 가게 전체조회
     List<Store> findAllByUser_Id(Long userId);
 
-    // 폐점이 아닌 가게 전체 조회
-    @Query("SELECT s FROM Store s WHERE s.status = 'NORMAL'")
-    List<Store> findAllByStatusNORMAL();
     // 가게 단건조회
     default Store findByIdOrElseThrow(Long storeId){
         return findById(storeId).orElseThrow(() -> new NotFoundException(ExceptionType.STORE_NOT_FOUND));
@@ -26,10 +23,7 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 
     // 사업자에게 등록된 사업장중 영업중인 사업장 갯수
     @Query("SELECT COUNT(s) FROM Store s WHERE s.user.id = :userId AND s.status = 'NORMAL'")
-    int findAllByUser_UserIdAndStatusNORMAL(@Param("userId") Long userId);
-
-    // 가게 아이디들으로 가게정보 반환
-    List<Store> findAllByStoreIdIn(List<Long> storeIds);
+    int findAllByUserIdAndStatusNORMAL(@Param("userId") Long userId);
 
     // 검색 기준에 따른 가게정보 호출 :: 생성일자
     @Query("SELECT s FROM Store s WHERE s.category =:category AND s.status = 'NORMAL' ORDER BY s.createdAt DESC ")
