f8ead94fa4f0fb3cdd1254688e8a78a74407500a
taketheking <so4524@gmail.com>
2024-12-08 15:44:19 +0900

    Fix : 이미지 없을 때 장바구니 조회시 오류 해결

--- src/main/java/com/kdg/hexa_delivery/domain/shoppingbasket/controller/ShoppingBasketController.java
+++ src/main/java/com/kdg/hexa_delivery/domain/shoppingbasket/controller/ShoppingBasketController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/shopping-baskets")
+@RequestMapping("/api/customers/shopping-baskets")
 public class ShoppingBasketController {
 
     private final ShoppingBasketService shoppingBasketService;
