456757fde0fbef54bb05d09e50ee331104b48aaa
정가현 <kahyun0608@gmail.com>
2024-12-08 18:25:32 +0900

    Feat : 소셜 로그인 api 구현

--- src/main/java/com/kdg/hexa_delivery/domain/user/SocialLogin/KakaoLoginController.java
+++ src/main/java/com/kdg/hexa_delivery/domain/user/SocialLogin/KakaoLoginController.java
@@ -2,12 +2,17 @@ package com.kdg.hexa_delivery.domain.user.SocialLogin;
 
 import com.kdg.hexa_delivery.domain.user.entity.User;
 import com.kdg.hexa_delivery.domain.user.service.UserService;
+import com.kdg.hexa_delivery.global.constant.Const;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -28,7 +33,7 @@ public class KakaoLoginController {
     }
 
     /**
-     * 카카오톡 소셜 로그인 요청 API -> 프론트에서 인가코드를 요청
+     * 카카오톡 소셜 로그인 요청 API : Model에 필요한 민감정보를 담아 프론트에 전달 -> 프론트에서 인가코드를 요청
      * https://kauth.kakao.com/oauth/authorize?client_id={$"kakao.api.key"}&redirect_uri=${kakao.redirect_uri}&response_type=code
      * -> /kakaoRedirect로 String code 형식으로 인가 코드를 담아서 보내준다.
      *
@@ -51,31 +56,49 @@ public class KakaoLoginController {
      * @return
      */
     @GetMapping("/kakaoRedirect")
-    public ResponseEntity<String> kakaoLogin(@RequestParam(required = false) String code) throws IOException, ParseException {
+    public ResponseEntity<String> kakaoLogin(@RequestParam(required = false) String code, HttpServletRequest servletRequest) throws IOException, ParseException {
+
+        //(테스트용) 잔류 세션 제거
+        servletRequest.getSession().invalidate();
 
         try {
+            //접근토큰 발급
             String accessToken = kakaoApi.getAccessToken(code);
 
-
+            //발급받은 토큰으로 사용자 정보 조회
             HashMap<String, Object> userInfo = kakaoLoginService.getUserInfo(accessToken);
 
+            //조회한 사용자 정보를 바탕으로 사용자 객체 생성
             User user = new User((String) userInfo.get("email"), (String) userInfo.get("nickname"));
 
+            //db에 사용자가 존재하지 않는다면 저장
             if (!userService.doesExistsByEmail(user.getEmail())) {
                 userService.saveKakaoUser(user);
             }
 
+            //현재의 세션 유무 확인
+            if (servletRequest.getSession(false) != null) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
             return ResponseEntity.status(HttpStatus.OK).body("로그인되었습니다.");
         } catch (IOException ioException) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("a");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당하는 정보를 찾을 수 없습니다.");
         }
     }
 
-    @PatchMapping("/kakao/{userId}")
-    public ResponseEntity<KakaoUserResponseDto> setRole(@PathVariable Long userId, @RequestBody SetUserRoleDto setUserRoleDto) {
+    @PostMapping("/kakao/{userId}")
+    public ResponseEntity<KakaoUserResponseDto> setRole(@PathVariable Long userId, @Valid @RequestBody SetUserRoleDto setUserRoleDto) {
         User userById = userService.findUserById(userId);
         userById.updateRole(setUserRoleDto.getRole());
         KakaoUserResponseDto kakaoUserResponseDto = new KakaoUserResponseDto(userId,userById.getEmail(),userById.getNickname(),userById.getRole());
         return ResponseEntity.status(HttpStatus.OK).body(kakaoUserResponseDto);
+    }
+
+    @PostMapping("/kakao/logout")
+    public ResponseEntity<String> kakaoLogout(@RequestHeader String Authorization){
+        kakaoLoginService.kakaoLogout(Authorization.substring(7));
+
+        return ResponseEntity.status(HttpStatus.OK).body("로그아웃되었습니다.");
     }
 }
